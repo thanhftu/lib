facba1fa4c56aff5bb428b76d93ca23ffce60f25
thanhftu <tcthanh.ftu@gmail.com>
2021-05-09 15:21:51 +0700

    add config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lib
 Library for myevent Service
+# format mongo connection string
+[mongodb://][user:pass@]host1[:port1][,host2[:port2],...]][/database][?option]
+
--- persistence/mongolayer/mongolayer.go
+++ persistence/mongolayer/mongolayer.go
@@ -2,7 +2,7 @@ package mongolayer
 
 import (
 	"github.com/thanhftu/lib/persistence"
-	"gopkg.in/mgo.v2"
+	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -12,63 +12,57 @@ const (
 	EVENTS = "events"
 )
 
-type MongoDBlayer struct {
+type MongoDBLayer struct {
 	session *mgo.Session
 }
 
-func NewMongoDBLayer(connection string) (*MongoDBlayer, error) {
+func NewMongoDBLayer(connection string) (persistence.DatabaseHandler, error) {
 	s, err := mgo.Dial(connection)
-	if err != nil {
-		return nil, err
-	}
-	return &MongoDBlayer{
+	return &MongoDBLayer{
 		session: s,
 	}, err
 }
 
-func (mgoLayer *MongoDBlayer) AddEvent(e persistence.Event) ([]byte, error) {
+func (mgoLayer *MongoDBLayer) AddEvent(e persistence.Event) ([]byte, error) {
 	s := mgoLayer.getFreshSession()
 	defer s.Close()
 
 	if !e.ID.Valid() {
 		e.ID = bson.NewObjectId()
 	}
+
 	if !e.Location.ID.Valid() {
 		e.Location.ID = bson.NewObjectId()
 	}
+
 	return []byte(e.ID), s.DB(DB).C(EVENTS).Insert(e)
 }
 
-func (mgoLayer *MongoDBlayer) FindEvent(id []byte) (persistence.Event, error) {
+func (mgoLayer *MongoDBLayer) FindEvent(id []byte) (persistence.Event, error) {
 	s := mgoLayer.getFreshSession()
 	defer s.Close()
-
-	e := persistence.Event{}                                    //create empty event object
-	err := s.DB(DB).C(EVENTS).FindId(bson.ObjectId(id)).One(&e) //e will obtain data of the retrieved document
-	return e, err
-}
-
-func (mgoLayer *MongoDBlayer) FindEventByName(name string) (persistence.Event, error) {
-	s := mgoLayer.getFreshSession()
-	defer s.Close()
-
 	e := persistence.Event{}
-	err := s.DB(DB).C(EVENTS).Find(bson.M{"name": name}).One(&e) //M standfor Map
+
+	err := s.DB(DB).C(EVENTS).FindId(bson.ObjectId(id)).One(&e)
 	return e, err
 }
 
-func (mgoLayer *MongoDBlayer) FindAllAvailableEvents() ([]persistence.Event, error) {
+func (mgoLayer *MongoDBLayer) FindEventByName(name string) (persistence.Event, error) {
 	s := mgoLayer.getFreshSession()
 	defer s.Close()
+	e := persistence.Event{}
+	err := s.DB(DB).C(EVENTS).Find(bson.M{"name": name}).One(&e)
+	return e, err
+}
 
+func (mgoLayer *MongoDBLayer) FindAllAvailableEvents() ([]persistence.Event, error) {
+	s := mgoLayer.getFreshSession()
+	defer s.Close()
 	events := []persistence.Event{}
-	err := s.DB(DB).C(EVENTS).Find(nil).All(&events) // Find(nil) return every thing found
+	err := s.DB(DB).C(EVENTS).Find(nil).All(&events)
 	return events, err
 }
 
-// getFreshSession helper method implemented in our code to
-// help retrieve a fresh database session from the connection pool
-
-func (mgoLayer *MongoDBlayer) getFreshSession() *mgo.Session {
+func (mgoLayer *MongoDBLayer) getFreshSession() *mgo.Session {
 	return mgoLayer.session.Copy()
 }
